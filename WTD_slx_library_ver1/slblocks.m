8914e04467f11154ad950481da753cb210c44cf7
Jakub Osmic <38258583+jacobianosmi@users.noreply.github.com>
2018-04-10 20:09:07 +0200

    Add files via upload

---
+++ WTD_slx_library_ver1/slblocks.m
@@ -0,0 +1,5 @@
+function blkStruct = slblocks
+  Browser.Library = 'Library0';  % this is name of library
+  Browser.Name    = 'WTD_Library_ver1_1';  % this is display name
+  Browser.IsFlat  = 0;% Is this library "flat" (i.e. no subsystems)?
+  blkStruct.Browser = Browser;
